db701d0d59315697f6d44caf8bc7252fec0dd9a4
PhucInAI <phucnguyenthanh.stu@gmail.com>
2023-12-05 17:01:18 +0700

    loss

---
+++ compute_loss.sh
@@ -0,0 +1,17 @@
+# ========================================================================
+# CVC-ClinicDB
+# ========================================================================
+# python train_LA.py -c configs/Loss_CVC-ClinicDB/LA_FFESNet_A2FPN_B0_drop0.5_CVC-ClinicDB_structure.yaml -d 0 && \
+# python train_LA.py -c configs/Loss_CVC-ClinicDB/LA_FFESNet_A2FPN_B0_drop0.5_CVC-ClinicDB_dice_bce.yaml -d 0 && \
+# python train_LA.py -c configs/Loss_CVC-ClinicDB/LA_FFESNet_A2FPN_B0_drop0.5_CVC-ClinicDB_tversky.yaml -d 0 && \
+python train_LA.py -c configs/Loss_CVC-ClinicDB/LA_FFESNet_A2FPN_B0_drop0.5_CVC-ClinicDB_tversky_bce.yaml -d 0 && \
+python train_LA.py -c configs/Loss_CVC-ClinicDB/LA_FFESNet_A2FPN_B0_drop0.5_CVC-ClinicDB_sufl.yaml -d 0
+
+# ========================================================================
+# Kvasir
+# ========================================================================
+python train_LA.py -c configs/Loss_Kvasir/LA_FFESNet_A2FPN_B0_drop0.5_Kvasir_structure.yaml -d 0 && \
+python train_LA.py -c configs/Loss_Kvasir/LA_FFESNet_A2FPN_B0_drop0.5_Kvasir_dice_bce.yaml -d 0 && \
+python train_LA.py -c configs/Loss_Kvasir/LA_FFESNet_A2FPN_B0_drop0.5_Kvasir_tversky.yaml -d 0 && \
+python train_LA.py -c configs/Loss_Kvasir/LA_FFESNet_A2FPN_B0_drop0.5_Kvasir_tversky_bce.yaml -d 0 && \
+python train_LA.py -c configs/Loss_Kvasir/LA_FFESNet_A2FPN_B0_drop0.5_Kvasir_sufl.yaml -d 0
--- loss_CVC-ClinicDB.sh
+++
@@ -1,7 +0,0 @@
-# python train_LA.py -c configs/Loss_CVC-ClinicDB/LA_FFESNet_A2FPN_B0_drop0.5_CVC-ClinicDB_structure.yaml -d 1 && \
-# python train_LA.py -c configs/Loss_CVC-ClinicDB/LA_FFESNet_A2FPN_B0_drop0.5_CVC-ClinicDB_dice_bce.yaml -d 1 && \
-# python train_LA.py -c configs/Loss_CVC-ClinicDB/LA_FFESNet_A2FPN_B0_drop0.5_CVC-ClinicDB_tversky.yaml -d 1 && \
-python train_LA.py -c configs/Loss_Kvasir/LA_FFESNet_A2FPN_B0_drop0.5_Kvasir_structure.yaml -d 1 && \
-python train_LA.py -c configs/Loss_Kvasir/LA_FFESNet_A2FPN_B0_drop0.5_Kvasir_dice_bce.yaml -d 1 && \
-python train_LA.py -c configs/Loss_CVC-ClinicDB/LA_FFESNet_A2FPN_B0_drop0.5_CVC-ClinicDB_tversky_bce.yaml -d 1 && \
-python train_LA.py -c configs/Loss_CVC-ClinicDB/LA_FFESNet_A2FPN_B0_drop0.5_CVC-ClinicDB_sufl.yaml -d 1
