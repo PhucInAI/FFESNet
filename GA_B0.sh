8ca419eb4db5f99ec1fe15d939d39f41a7fdbecd
PhucInAI <phucnguyenthanh.stu@gmail.com>
2023-12-09 03:53:43 +0700

    GA

---
+++ GA_B0.sh
@@ -0,0 +1,13 @@
+python train_GA_n_PB.py -c /home/ptn/Storage/FFESNet/configs/GA_B0_drop0.2/FFESNet_A2FPN_B0_drop0.2_Kvasir.yaml -d 0 -t GA && \
+python train_GA_n_PB.py -c /home/ptn/Storage/FFESNet/configs/GA_B0_drop0.2/FFESNet_LE_A2FPN_B0_drop0.2_Kvasir.yaml -d 0 -t GA && \
+python train_GA_n_PB.py -c /home/ptn/Storage/FFESNet/configs/GA_B0_drop0.2/FFESNet_PAN_B0_drop0.2_Kvasir.yaml -d 0 -t GA && \
+python train_GA_n_PB.py -c /home/ptn/Storage/FFESNet/configs/GA_B0_drop0.2/FFESNet_LE_PAN_B0_drop0.2_Kvasir.yaml -d 0 -t GA && \
+python train_GA_n_PB.py -c /home/ptn/Storage/FFESNet/configs/GA_B0_drop0.2/FFESNet_PAN_AttentionAggregation_B0_drop0.2_Kvasir.yaml -d 0 -t GA && \
+python train_GA_n_PB.py -c /home/ptn/Storage/FFESNet/configs/GA_B0_drop0.2/FFESNet_LE_PAN_AttentionAggregation_B0_drop0.2_Kvasir.yaml -d 0 -t GA
+
+python train_GA_n_PB.py -c /home/ptn/Storage/FFESNet/configs/GA_B0_drop0.5/FFESNet_A2FPN_B0_drop0.5_Kvasir.yaml -d 0 -t GA && \
+python train_GA_n_PB.py -c /home/ptn/Storage/FFESNet/configs/GA_B0_drop0.5/FFESNet_LE_A2FPN_B0_drop0.5_Kvasir.yaml -d 0 -t GA && \
+python train_GA_n_PB.py -c /home/ptn/Storage/FFESNet/configs/GA_B0_drop0.5/FFESNet_PAN_B0_drop0.5_Kvasir.yaml -d 0 -t GA && \
+python train_GA_n_PB.py -c /home/ptn/Storage/FFESNet/configs/GA_B0_drop0.5/FFESNet_LE_PAN_B0_drop0.5_Kvasir.yaml -d 0 -t GA && \
+python train_GA_n_PB.py -c /home/ptn/Storage/FFESNet/configs/GA_B0_drop0.5/FFESNet_PAN_AttentionAggregation_B0_drop0.5_Kvasir.yaml -d 0 -t GA && \
+python train_GA_n_PB.py -c /home/ptn/Storage/FFESNet/configs/GA_B0_drop0.5/FFESNet_LE_PAN_AttentionAggregation_B0_drop0.5_Kvasir.yaml -d 0 -t GA
