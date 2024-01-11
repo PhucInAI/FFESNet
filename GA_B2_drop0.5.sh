72be1c70ec0e55996f9fccd69ac54be30079c4a6
PhucInAI <phucnguyenthanh.stu@gmail.com>
2024-01-11 11:37:07 +0700

    backup thesis

--- GA_B2_drop0.5.sh
+++ GA_B2_drop0.5.sh
@@ -1,5 +1,6 @@
-python train_GA_n_PB.py -c /home/ptn/Storage/FFESNet/configs/GA_B2_drop0.5/FFESNet_A2FPN_B2_drop0.5_Kvasir.yaml -d 1 -t GA && \
-python train_GA_n_PB.py -c /home/ptn/Storage/FFESNet/configs/GA_B2_drop0.5/FFESNet_LE_A2FPN_B2_drop0.5_Kvasir.yaml -d 1 -t GA && \
+# python train_GA_n_PB.py -c /home/ptn/Storage/FFESNet/configs/GA_B2_drop0.5/FFESNet_A2FPN_B2_drop0.5_Kvasir.yaml -d 1 -t GA && \
+# python train_GA_n_PB.py -c /home/ptn/Storage/FFESNet/configs/GA_B2_drop0.5/FFESNet_LE_A2FPN_B2_drop0.5_Kvasir.yaml -d 1 -t GA && \
+python train_GA_n_PB.py -c /home/ptn/Storage/FFESNet/configs/GA_B2_drop0.2/FFESNet_A2FPN_B2_drop0.2_Kvasir.yaml -d 1 -t GA && \
 python train_GA_n_PB.py -c /home/ptn/Storage/FFESNet/configs/GA_B2_drop0.5/FFESNet_PAN_B2_drop0.5_Kvasir.yaml -d 1 -t GA && \
 python train_GA_n_PB.py -c /home/ptn/Storage/FFESNet/configs/GA_B2_drop0.5/FFESNet_LE_PAN_B2_drop0.5_Kvasir.yaml -d 1 -t GA && \
 python train_GA_n_PB.py -c /home/ptn/Storage/FFESNet/configs/GA_B2_drop0.5/FFESNet_PAN_AttentionAggregation_B2_drop0.5_Kvasir.yaml -d 1 -t GA && \
